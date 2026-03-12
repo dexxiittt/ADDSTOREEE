c8d5ad68f478ca2140eb67048f4b627af999ee12
addsite <dexxiittt@gmail.com>
2026-03-13 00:37:50 +0700

    Update premium-cta.js

--- components/premium-cta.js
+++ components/premium-cta.js
@@ -1,5 +1,9 @@
 fetch("components/premium-cta.html")
-.then(res => res.text())
-.then(data => {
-document.getElementById("premium-cta").innerHTML = data;
-});
+  .then(res => {
+    if(!res.ok) throw new Error("CTA load error");
+    return res.text();
+  })
+  .then(data => {
+    document.getElementById("premium-cta").innerHTML = data;
+  })
+  .catch(err => console.error(err));
