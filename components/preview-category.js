d85ca590ea184bf197e1b480b6c55afb328aa32b
addsite <dexxiittt@gmail.com>
2026-07-02 10:33:46 +0700

    Update preview-category.js

--- components/preview-category.js
+++ components/preview-category.js
@@ -1,1 +1,21 @@
+function initPreviewCategory(){
 
+    const tabs=document.querySelectorAll(".preview-tabs button");
+
+    tabs.forEach(tab=>{
+
+        tab.onclick=()=>{
+
+            tabs.forEach(t=>t.classList.remove("active"));
+
+            tab.classList.add("active");
+
+            loadPreview(tab.dataset.category);
+
+        };
+
+    });
+
+    loadPreview("premium");
+
+}
