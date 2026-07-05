009619ead0c1f58a5e6f3654bcacf0016515808b
addsite <dexxiittt@gmail.com>
2026-07-06 00:27:53 +0700

    Update status-pembayaran.js

--- components/status-pembayaran/status-pembayaran.js
+++ components/status-pembayaran/status-pembayaran.js
@@ -2,52 +2,6 @@ let rawInvoice = "";
 
 window.onload = async function(){
 
-// =======================
-// CUSTOMER
-// =======================
-
-const customer =
-await getCustomerData();
-
-
-
-// =======================
-// PRODUCT
-// =======================
-
-const product =
-await getProductData(customer);
-
-
-
-// =======================
-// RENDER
-// =======================
-
-renderCustomer(customer);
-
-renderProduct(product);
-
-renderInvoice(customer);
-
-
-
-// =======================
-// STATUS
-// =======================
-
-const paymentStatus =
-await getPaymentStatus(customer.invoice);
-
-renderStatus(paymentStatus);
-
-}
-
-
-let rawInvoice = "";
-
-window.onload = async function(){
-
 // =============================
 // PRIORITAS: AMBIL DARI LOCAL STORAGE
 // =============================
