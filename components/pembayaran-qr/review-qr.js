5eda6f9ef13064304aa3368097ad30d12a49c085
addsite <dexxiittt@gmail.com>
2026-07-06 19:22:00 +0700

    Update review-qr.js

--- components/pembayaran-qr/review-qr.js
+++ components/pembayaran-qr/review-qr.js
@@ -4,15 +4,10 @@ window.onload = async function(){
 // CUSTOMER
 // =======================
 
-function getCustomerData(){
-
 const customer = getCustomerData();
 
 renderCustomer(customer);
-
   
-}
-
 
 // =======================
 // QR
@@ -22,6 +17,50 @@ await loadQRCode();
 
 }
 
+// =======================
+// CUSTOMER
+// =======================
+
+function getCustomerData(){
+
+const customer =
+JSON.parse(localStorage.getItem("paymentData"));
+
+if(!customer){
+
+alert("Data tidak ditemukan, kembali ke halaman sebelumnya");
+
+window.location.href = "opsi-pembayaran.html";
+
+}
+
+return customer;
+
+}
+
+function renderCustomer(customer){
+
+document.getElementById("nama").innerText = customer.nama;
+
+document.getElementById("telepon").innerText = customer.telepon;
+
+document.getElementById("email").innerText = customer.email;
+
+document.getElementById("paket").innerText = customer.paket;
+
+document.getElementById("paketDetail").innerText = customer.paketDetail;
+
+document.getElementById("total").innerText = customer.total;
+
+document.getElementById("paket-harga").innerHTML = customer.paketHarga;
+
+document.getElementById("paket-diskon").innerText = customer.diskon;
+
+document.getElementById("paket-hemat").innerText = customer.hemat;
+
+document.getElementById("paket-img").src = customer.image;
+  
+}
 
 // =======================
 // EVENT
