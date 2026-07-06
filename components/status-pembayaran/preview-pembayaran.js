0cf2e7d00f8743d246d006e2fc8ec161f1e80353
addsite <dexxiittt@gmail.com>
2026-07-06 07:34:17 +0700

    Update preview-pembayaran.js

--- components/status-pembayaran/preview-pembayaran.js
+++ components/status-pembayaran/preview-pembayaran.js
@@ -1,1 +1,44 @@
+let rawInvoice = "";
 
+window.onload = async function(){
+
+// =======================
+// CUSTOMER
+// =======================
+
+const customer =
+await getCustomerData();
+
+
+
+// =======================
+// PRODUCT
+// =======================
+
+const product =
+await getProductData(customer);
+
+
+
+// =======================
+// RENDER
+// =======================
+
+renderCustomer(customer);
+
+renderProduct(product);
+
+renderInvoice(customer);
+
+
+
+// =======================
+// STATUS
+// =======================
+
+const paymentStatus =
+await getPaymentStatus(customer.invoice);
+
+renderStatus(paymentStatus);
+
+}
