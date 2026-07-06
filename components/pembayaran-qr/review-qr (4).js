f5b4bbb8114cdba66ca90e37c62a131a485f4771
addsite <dexxiittt@gmail.com>
2026-07-06 20:01:41 +0700

    Add files via upload

---
+++ components/pembayaran-qr/review-qr (4).js
@@ -0,0 +1,146 @@
+window.onload = async function(){
+
+// =======================
+// CUSTOMER
+// =======================
+
+const customer = getCustomerData();
+
+renderCustomer(customer);
+  
+
+// =======================
+// CUSTOMER
+// =======================
+
+function getCustomerData(){
+
+const customer =
+JSON.parse(localStorage.getItem("paymentData"));
+
+if(!customer){
+
+alert("Data tidak ditemukan, kembali ke halaman sebelumnya");
+
+window.location.href = "opsi-pembayaran.html";
+
+}
+
+return customer;
+
+}
+
+function renderCustomer(customer){
+
+document.getElementById("nama").innerText = customer.nama;
+
+document.getElementById("telepon").innerText = customer.telepon;
+
+document.getElementById("email").innerText = customer.email;
+
+document.getElementById("paket").innerText = customer.paket;
+
+document.getElementById("paketDetail").innerText = customer.paketDetail;
+
+document.getElementById("total").innerText = customer.total;
+
+document.getElementById("paket-harga").innerHTML = customer.paketHarga;
+
+document.getElementById("paket-diskon").innerText = customer.diskon;
+
+document.getElementById("paket-hemat").innerText = customer.hemat;
+
+document.getElementById("paket-img").src = customer.image;
+  
+}
+
+
+// =======================
+// QR
+// =======================
+
+async function loadQRCode(){
+
+const qrSheet =
+"https://opensheet.elk.sh/1JtmaN7ASwvnQzoOKPqVA3Uy85fcNfcLTArYOyQZRV08/qr_id"
+
+  fetch(qrSheet)
+  .then(res => res.json())
+    
+  .then(data => {
+
+// ambil baris pertama
+const qr = data[0]?.qr_code
+
+if(qr){
+document.getElementById("qr-img").src = qr
+}else{
+document.getElementById("qr-img").src =
+"https://via.placeholder.com/220?text=QR+Not+Found"
+}
+
+})
+    
+.catch(()=>{
+
+document.getElementById("qr-img").src =
+"https://via.placeholder.com/220?text=Error"
+
+});
+}
+  
+// =======================
+// QR
+// =======================
+
+await loadQRCode();
+
+}
+
+
+// =======================
+// EVENT
+// =======================
+
+function cekStatus(){
+
+const invoice = getInvoice();
+
+redirectStatus(invoice);
+
+}
+
+
+// =======================
+// INVOICE
+// =======================
+
+function getInvoice(){
+
+}
+
+function generateInvoice(){
+
+}
+
+
+// =======================
+// NAVIGATION
+// =======================
+
+function kembaliProduk(){
+
+}
+
+
+// =======================
+// QR MODAL
+// =======================
+
+function openQR(){
+
+}
+
+function closeQR(){
+
+}
