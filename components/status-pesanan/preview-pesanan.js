7675d133040e27f24da5f81a6daaf276b9f7c27a
addsite <dexxiittt@gmail.com>
2026-07-08 21:45:14 +0700

    Update preview-pesanan.js

--- components/status-pesanan/preview-pesanan.js
+++ components/status-pesanan/preview-pesanan.js
@@ -1,1 +1,21 @@
+// =============================
+// AMBIL PARAMETER INVOICE
+// =============================
 
+const params = new URLSearchParams(window.location.search);
+
+const invoice = params.get("inv");
+
+if (!invoice) {
+  window.location.href = "invoice.html";
+}
+
+// =============================
+// AMBIL ELEMENT UTAMA
+// =============================
+
+const wrapper = document.querySelector(".status-wrapper");
+
+// =============================
+// STEP 1.3
+// =============================
