9e6d32e15e45bd2078865e3343db384c0214333b
addsite <dexxiittt@gmail.com>
2026-07-16 17:10:11 +0700

    Update chat-admin.js

--- components/utils/chat-admin.js
+++ components/utils/chat-admin.js
@@ -15,6 +15,21 @@ window.open(
 function chatAdminTransaksi(data){
 
 console.log("Nama :", data.buyerName);
-console.log("Nomor:", data.buyerPhone);
+
+console.log("Nomor :", data.buyerPhone);
+
+console.log("Invoice :", data.invoice);
+
+console.log("Produk :", data.product);
+
+console.log("Paket :", data.package);
+
+console.log("Aktivasi :", data.activatedDate);
+
+console.log("Paket Berakhir :", data.packageExpired);
+
+console.log("Garansi Berakhir :", data.warrantyExpired);
+
+console.log("Paket Aktif :", data.packageActive);
 
 }
