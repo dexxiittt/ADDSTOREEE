2494df77ee527852e7a524340b2a399a2bbfa815
addsite <dexxiittt@gmail.com>
2026-07-16 16:32:25 +0700

    Update chat-admin.js

--- components/utils/chat-admin.js
+++ components/utils/chat-admin.js
@@ -1,1 +1,19 @@
+const ADMIN_NUMBER = "6285881500868";
 
+function openWhatsApp(message){
+
+window.open(
+
+`https://wa.me/${ADMIN_NUMBER}?text=${encodeURIComponent(message)}`,
+
+"_blank"
+
+);
+
+}
+
+function chatAdminTransaksi(data){
+
+console.log(data);
+
+}
